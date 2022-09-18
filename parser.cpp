147370f8151cf41b9b4525684e02a28915306b82
tyc5 <ivan30103@gmail.com>
2022-09-18 09:46:15 +0800

    modified parser

--- parser.cpp
+++ parser.cpp
@@ -2,4 +2,14 @@
 
 void Parser::parser(std::string filename) {
     std::cout << ">> Parsing...\n";
+    std::ifstream in_file;
+    in_file.open(filename, std::ios::in);
+    if (!in_file) {
+        std::cerr << "Failed to open file: " << filename << ".\n";
+        return;
+    }
+
+    std::string line, str;
+    
+    in_file.close();
 }
