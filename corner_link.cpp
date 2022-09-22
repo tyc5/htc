2a6d13a3f0a200872679f85a4a3a4c6cae68c822
tyc5 <ivan30103@gmail.com>
2022-09-22 11:42:03 +0800

    add corner_link

---
+++ corner_link.cpp
@@ -0,0 +1,5 @@
+#include "corner_link.h"
+
+void CornerLink::init(Data& data) {
+    std::cout << "\n>> Corner Link Init...\n";
+}
---
+++ corner_link.h
@@ -0,0 +1,11 @@
+#ifndef _CORNER_LINK_H
+#define _CORNER_LINK_H
+
+#include "parser.h"
+
+class CornerLink {
+    public:
+        void init(Data& data);
+};
+
+#endif
