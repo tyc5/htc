d63393cf99bb7d7c4bcb53cd764694d55e4f6c55
tyc5 <ivan30103@gmail.com>
2022-09-27 01:05:12 +0800

    Update: format

--- parser.h
+++ parser.h
@@ -1,28 +1,28 @@
 #ifndef _PARSER_H
 #define _PARSER_H
 
-#include <iostream>
 #include <fstream>
-#include <string>
-#include <vector>
-#include <sstream>
-#include <unordered_map>
+#include <iostream>
 #include <map>
+#include <sstream>
+#include <string>
+#include <unordered_map>
+#include <vector>
 
 struct Point {
     double x;
     double y;
     double z;
-    Point(): x(0), y(0), z(0) {}
-    Point(double x_, double y_, double z_): x(x_), y(y_), z(z_) {}
+    Point() : x(0), y(0), z(0) {}
+    Point(double x_, double y_, double z_) : x(x_), y(y_), z(z_) {}
 };
 
 struct Corners {
     // -: n, +: p
-    // opposite corners: {− − −(nnn), − + +(npp), + − +(pnp), + + −(ppn)}
-    Point nnn, npp, pnp, ppn;
-    // linking corners:  {− − +(nnp), − + −(npn), + − −(pnn), + + +(ppp)}
+    // opposite corners:  {− − +(nnp), − + −(npn), + − −(pnn), + + +(ppp)}
     Point nnp, npn, pnn, ppp;
+    // linking corners: {− − −(nnn), − + +(npp), + − +(pnp), + + −(ppn)}
+    Point nnn, npp, pnp, ppn;
 };
 
 struct Block {
@@ -38,28 +38,29 @@ struct Block {
 struct Space {
     std::string unit;
     Block space;
-    Space(): unit(""), space() {}
-    Space(std::string unit_, Block space_): unit(unit_), space(space_) {}
+    Space() : unit(""), space() {}
+    Space(std::string unit_, Block space_) : unit(unit_), space(space_) {}
 };
 
 struct SolidBlock {
     Block solid;
     std::string material;
     double emissivity[6];
-    SolidBlock(): solid(), material("") { for (int i = 0; i < 6; ++i) emissivity[i] = 0;}
+    SolidBlock() : solid(), material("") {
+        for (int i = 0; i < 6; ++i) emissivity[i] = 0;
+    }
 };
 
 class Data {
-    public:
-        Space place_space;
-        std::vector<SolidBlock> solid_blocks;
-        
+public:
+    Space place_space;
+    std::vector<SolidBlock> solid_blocks;
 };
 
 class Parser {
-    public:
-        void parser(std::string filename, Data& data);
-        void print_info(Data& data);
+public:
+    void parser(std::string filename, Data& data);
+    void print_info(Data& data);
 };
 
 #endif
