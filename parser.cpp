00dd5ec11f7adf7934c04751658d1e83e1ec0af8
tyc5 <ivan30103@gmail.com>
2022-09-26 15:52:17 +0800

    modified parser

--- parser.cpp
+++ parser.cpp
@@ -11,27 +11,34 @@ void Parser::parser(std::string filename, Data& data) {
 
     std::string str;
     // Place Space
-    Space place_space;
+    Space &place_space = data.place_space;
     in_file >> str >> place_space.unit;
-    std::cout << "place unit: " << place_space.unit << std::endl;
+    
     in_file >> str >> place_space.space.name;
-    std::cout << "place name: " << place_space.space.name << std::endl;
+    
     in_file >> str
             >> place_space.space.loc.x
             >> place_space.space.loc.y
             >> place_space.space.loc.z;
-    printf("place location: (%f, %f, %f)\n"
-          , place_space.space.loc.x
-          , place_space.space.loc.y
-          , place_space.space.loc.z);
+    
     in_file >> str
             >> place_space.space.len_x
             >> place_space.space.len_y
             >> place_space.space.len_z;
-    printf("place geometry: (%f, %f, %f)\n"
-          , place_space.space.len_x
-          , place_space.space.len_y
-          , place_space.space.len_z);
+    
+    Block &space = data.place_space.space;
+    Point &space_loc = data.place_space.space.loc;
+    Corners &space_corners = data.place_space.space.corners;
+    // opposite corners
+    space.corners.nnn = Point(space_loc.x, space_loc.y, space_loc.z);
+    space.corners.npp = Point(space_loc.x, space_loc.y + space.len_y, space_loc.z + space.len_z);
+    space.corners.pnp = Point(space_loc.x + space.len_x, space_loc.y, space_loc.z + space.len_z);
+    space.corners.ppn = Point(space_loc.x + space.len_x, space_loc.y + space.len_y, space_loc.z);
+    // linking corners
+    space.corners.nnp = Point(space_loc.x, space_loc.y, space_loc.z + space.len_z);
+    space.corners.npn = Point(space_loc.x, space_loc.y + space.len_y, space_loc.z);
+    space.corners.pnn = Point(space_loc.x + space.len_x, space_loc.y, space_loc.z);
+    space.corners.ppp = Point(space_loc.x + space.len_x, space_loc.y + space.len_y, space_loc.z + space.len_z);
     std::cout << std::endl;
     // Solid Block
     SolidBlock solid_block;
@@ -56,17 +63,30 @@ void Parser::parser(std::string filename, Data& data) {
         for (int i = 0; i < 6; ++i) {
             in_file >> solid_block.emissivity[i];
         }
-        // for (int i = 0; i <= 6; ++i) in_file >> str;
-        // for (int i = 0; i < 6; ++i) {
-        //     double e;
-        //     in_file >> e;
-        //     solid_block.emissivity[i] = e;
-        // }
         in_file >> str;
-                
+
+        // corners
+        double &x = solid_block.solid.loc.x;
+        double &y = solid_block.solid.loc.y;
+        double &z = solid_block.solid.loc.z;
+        double &len_x = solid_block.solid.len_x;
+        double &len_y = solid_block.solid.len_y;
+        double &len_z = solid_block.solid.len_z;
+        Corners &corners = solid_block.solid.corners;
+        // opposite corners
+        corners.nnn = Point(x, y, z);
+        corners.npp = Point(x, y + len_y, z + len_z);
+        corners.pnp = Point(x + len_x, y, z + len_z);
+        corners.ppn = Point(x + len_x, y + len_y, z);
+        // linking corners
+        corners.nnp = Point(x, y, z + len_z);
+        corners.npn = Point(x, y + len_y, z);
+        corners.pnn = Point(x + len_x, y, z);
+        corners.ppp = Point(x + len_x, y + len_y, z + len_z);
+
         data.solid_blocks.emplace_back(solid_block);
     }
-    
+    /*
     for (const auto& b: data.solid_blocks) {
         printf("\nName: %s\nLocation: (%f, %f, %f)\nGeometry: (%f, %f, %f)\nMaterial: %s\nEmissivity:"
             , b.solid.name.c_str()
@@ -80,5 +100,58 @@ void Parser::parser(std::string filename, Data& data) {
         for (int i = 0; i < 6; ++i) std::cout << " " << b.emissivity[i];
         std::cout << std::endl;
     }
+    */
     in_file.close();
+}
+
+void Parser::print_info(Data& data) {
+    std::cout << "\n>>> Print Info...\n";
+    std::cout << ">>>> Place Space Info: \n";
+    Space &place_space = data.place_space;
+    std::cout << "place unit: " << place_space.unit << std::endl;
+    std::cout << "place name: " << place_space.space.name << std::endl;
+    printf("place location: (%f, %f, %f)\n"
+          , place_space.space.loc.x
+          , place_space.space.loc.y
+          , place_space.space.loc.z);
+    printf("place geometry: (%f, %f, %f)\n"
+          , place_space.space.len_x
+          , place_space.space.len_y
+          , place_space.space.len_z);
+    
+    std::cout << "\n>>>> Blocks Info: \n";
+    for (const auto& b: data.solid_blocks) {
+        double x = b.solid.loc.x;
+        double y = b.solid.loc.y;
+        double z = b.solid.loc.z;
+        double len_x = b.solid.len_x;
+        double len_y = b.solid.len_y;
+        double len_z = b.solid.len_z;
+        Corners corners = b.solid.corners;
+        std::cout << "\nBlock: " << b.solid.name << std::endl;
+        printf("---: (%f, %f, %f)\n", x, y, z);
+        printf("+--: (%f, %f, %f)\n", x + len_x, y, z);
+        printf("-+-: (%f, %f, %f)\n", x, y + len_y, z);
+        printf("--+: (%f, %f, %f)\n", x, y, z + len_z);
+        printf("++-: (%f, %f, %f)\n", x + len_x, y + len_y, z);
+        printf("-++: (%f, %f, %f)\n", x, y + len_y, z + len_z);
+        printf("+-+: (%f, %f, %f)\n", x + len_x, y, z + len_z);
+        printf("+++: (%f, %f, %f)\n", x + len_x, y + len_y, z + len_z);
+        
+        // opposite corners
+        std::cout << "\nopposite corners: \n";
+        printf("nnn: (%f, %f, %f)\n", corners.nnn.x, corners.nnn.y, corners.nnn.z);
+        printf("npp: (%f, %f, %f)\n", corners.npp.x, corners.npp.y, corners.npp.z);
+        printf("pnp: (%f, %f, %f)\n", corners.pnp.x, corners.pnp.y, corners.pnp.z);
+        printf("ppn: (%f, %f, %f)\n", corners.ppn.x, corners.ppn.y, corners.ppn.z);
+
+        // linking corners
+        std::cout << "\nlinking corners: \n";
+        printf("nnp: (%f, %f, %f)\n", corners.nnp.x, corners.nnp.y, corners.nnp.z);
+        printf("npn: (%f, %f, %f)\n", corners.npn.x, corners.npn.y, corners.npn.z);
+        printf("pnn: (%f, %f, %f)\n", corners.pnn.x, corners.pnn.y, corners.pnn.z);
+        printf("ppp: (%f, %f, %f)\n", corners.ppn.x, corners.ppn.y, corners.ppn.z);
+        
+        // 
+    }
 }
--- parser.h
+++ parser.h
@@ -6,11 +6,23 @@
 #include <string>
 #include <vector>
 #include <sstream>
+#include <unordered_map>
+#include <map>
 
 struct Point {
     double x;
     double y;
     double z;
+    Point(): x(0), y(0), z(0) {}
+    Point(double x_, double y_, double z_): x(x_), y(y_), z(z_) {}
+};
+
+struct Corners {
+    // -: n, +: p
+    // opposite corners: {− − −(nnn), − + +(npp), + − +(pnp), + + −(ppn)}
+    Point nnn, npp, pnp, ppn;
+    // linking corners:  {− − +(nnp), − + −(npn), + − −(pnn), + + +(ppp)}
+    Point nnp, npn, pnn, ppp;
 };
 
 struct Block {
@@ -19,28 +31,35 @@ struct Block {
     double len_x;
     double len_y;
     double len_z;
+    Corners corners;
 };
 
 // Place space
 struct Space {
     std::string unit;
     Block space;
+    Space(): unit(""), space() {}
+    Space(std::string unit_, Block space_): unit(unit_), space(space_) {}
 };
 
 struct SolidBlock {
     Block solid;
     std::string material;
-    double emissivity[6];  
+    double emissivity[6];
+    SolidBlock(): solid(), material("") { for (int i = 0; i < 6; ++i) emissivity[i] = 0;}
 };
 
 class Data {
     public:
+        Space place_space;
         std::vector<SolidBlock> solid_blocks;
+        
 };
 
 class Parser {
     public:
         void parser(std::string filename, Data& data);
+        void print_info(Data& data);
 };
 
 #endif
