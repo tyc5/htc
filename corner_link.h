36f5aed6fc8e76c432cccc88dc87ebe7b76cdb4a
tyc5 <ivan30103@gmail.com>
2022-10-18 15:21:32 +0800

    Modified: type

--- corner_link.h
+++ corner_link.h
@@ -9,10 +9,12 @@ class CornerLink {
 public:
     void get_corner_link(Data& data);
     void print_blocks(Data& data, int mode);
-    bool same_coordi(auto& v, auto& w);
-    std::string coordi_info(auto& v);
-    int get_ham_dist(auto& v, auto& w);
-    // boomal corner_pair_exist(auto& cp1, auto& cp2);
+    bool same_coordi(std::pair<const std::string, Point>& v,
+                     std::pair<const std::string, Point>& w);
+    std::string coordi_info(std::pair<const std::string, Point>& v);
+    int get_ham_dist(std::pair<const std::string, Point>& v,
+                     std::pair<const std::string, Point>& w);
+    bool corner_pair_exist(auto& cp1, auto& cp2);
 };
 
 #endif
