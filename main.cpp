b4838c79fa975504f8d7c6bd32ad5e8ccc038448
tyc5 <ivan30103@gmail.com>
2022-10-20 20:17:44 +0800

    Modified: debug

--- main.cpp
+++ main.cpp
@@ -19,8 +19,8 @@ int main(int argc, char* argv[]) {
     std::cout << "\n/----- Case: " << argv[1] << " -----/\n";
 
     parser.parser(argv[1], data);
-    // parser.print_info(data);
-    corner_link.get_corner_link(data);
+    parser.print_info(data);
+    // corner_link.get_corner_link(data);
 
     time_t end = clock();
     float t_used = (float)(end - start) / CLOCKS_PER_SEC;
