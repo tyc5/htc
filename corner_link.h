af6404ee5c0a47e3602edaed95d2b60fd0b020bc
tyc5 <ivan30103@gmail.com>
2022-10-24 00:38:02 +0800

    Modified: corner_pair_exist type

--- corner_link.h
+++ corner_link.h
@@ -6,7 +6,8 @@
 
 #include "parser.h"
 
-// typedef std::pair<typename T1, typename T2>
+typedef std::pair<Block, std::string> corner;
+typedef std::pair<corner, corner> corner_pair;
 
 class CornerLink {
 public:
@@ -17,7 +18,7 @@ public:
     std::string coordi_info(std::pair<const std::string, Point>& v);
     int get_ham_dist(std::pair<const std::string, Point>& v,
                      std::pair<const std::string, Point>& w);
-    bool corner_pair_exist(auto& cp1, auto& cp2);
+    bool corner_pair_exist(corner_pair& cp1, corner_pair& cp2);
 };
 
 #endif
