3a3940c7241d9ffe315b0492ac37f4bcf0fc8f3c
tyc5 <ivan30103@gmail.com>
2022-10-27 17:42:11 +0800

    Modified: PartialOrder

---
+++ partial_order.h
@@ -0,0 +1,12 @@
+#ifndef _PARTIAL_ORDER_H
+#define _PARTIAL_ORDER_H
+
+#include "parser.h"
+#include "corner_link.h"
+
+class PartialOrder {
+public:
+    void get_partial_order(Data& data);
+};
+
+#endif
