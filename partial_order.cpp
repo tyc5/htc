9c15e50f10b21d3f94c9bc1b6522ca3b6fb8af20
tyc5 <ivan30103@gmail.com>
2022-11-30 01:49:29 +0800

    Add: Graph

--- partial_order.cpp
+++ partial_order.cpp
@@ -1,6 +1,22 @@
 #include "partial_order.h"
+#include <typeinfo>
+#include <cxxabi.h>
+
+void Graph::add_edge_list(Block& from, Block& to) {
+    std::cout << "type of from: "
+              << abi::__cxa_demangle(typeid(from).name(), 0, 0, 0) << std::endl;
+    std::cout << "type of to: "
+              << abi::__cxa_demangle(typeid(to).name(), 0, 0, 0) << std::endl;
+
+    std::cout << "type of adj_list[from]: "
+              << abi::__cxa_demangle(typeid(adj_list[from.name]).name(), 0, 0, 0) << std::endl;
+
+    adj_list[from.name].emplace_back(to);
+}
 
 void PartialOrder::get_partial_order(Data& data) {
+    std::cout << "type of data: "
+              << abi::__cxa_demangle(typeid(data).name(), 0, 0, 0) << std::endl;
     // initialize stitching_planes
     data.stitching_planes.reserve(3);
     data.stitching_planes = {{}, {}, {}};
@@ -15,11 +31,17 @@ void PartialOrder::get_partial_order(Data& data) {
             data.stitching_planes[2].emplace(cl.first.z);
     }
 
+    Graph partial_order_x;
+    partial_order_x.add_edge_list(data.blocks[0], data.blocks[1]);
+
     for (int i = 0; i < data.stitching_planes.size(); ++i) {
         std::string str = i ? (i == 1 ? "y: " : "z: ") : "x: ";
         std::cout << str << std::endl;
-        for (const auto& p: data.stitching_planes[i])
+        for (const auto& p: data.stitching_planes[i]) {
             std::cout << p << std::endl;
+
+
+        }
         std::cout << std::endl;
     }
 }
--- partial_order.h
+++ partial_order.h
@@ -4,6 +4,14 @@
 #include "parser.h"
 #include "corner_link.h"
 
+class Graph {
+private:
+    std::unordered_map<std::string, std::vector<Block>> adj_list;
+
+public:
+    void add_edge_list(Block& from, Block& to);
+};
+
 class PartialOrder {
 public:
     void get_partial_order(Data& data);
